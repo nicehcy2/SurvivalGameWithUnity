4d8628e00da081acbc6f96d8190438d9a9f8c14f
Chanyoung Heo <nicehcy2@naver.com>
2023-01-14 15:27:19 +0900

    프로젝트 기본적인 구조
    23.01.14 By Chan-Young Heo

---
+++ Assets/Undead Survivor/Scripts/Enemy.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed;
+    public float health;
+    public float maxHealth;
+    //public RuntimeAnimatorController[] animCon;
+    public Rigidbody2D target;
+
+    bool isLive;
+    Rigidbody2D rigid;
+    SpriteRenderer spriter;
+    Animator anim;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        //anim = GetComponent<Animator>();
+        spriter = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (!isLive)
+            return;
+
+        Vector2 dirVec = target.position - rigid.position;
+        Vector2 nextVec = dirVec.normalized * speed * Time.fixedDeltaTime;
+        rigid.MovePosition(rigid.position + nextVec);
+        rigid.velocity = Vector2.zero;
+    }
+
+    void LateUpdate()
+    {
+        if (!isLive)
+            return;
+        spriter.flipX = target.position.x < rigid.position.x;
+    }
+
+    void OnEnable()
+    {
+        target = GameManager.instance.player.GetComponent<Rigidbody2D>();
+        isLive = true;
+        health = maxHealth;
+    }
+
+    public void Init(SpawnData data)
+    {
+        //anim.runtimeAnimatorController = animCon[data.spriteType];
+        speed = data.speed;
+        maxHealth = data.health;
+        health = data.health;
+    }
+}
